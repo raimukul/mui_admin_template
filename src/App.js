4aae423a0d0b5dc1a14db2e629a931bcdf367a10
Mukul Rai <47410631+raimukul@users.noreply.github.com>
2023-05-31 19:03:35 -0500

    update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
-// project import
-import Routes from 'routes';
+import { lazy } from 'react';
+const Dashboard = Loadable(lazy(() => import('pages/dashboard/index')));
+const Login = Loadable(lazy(() => import('pages/authentication/Login')));
+const SamplePage = Loadable(lazy(() => import('pages/extra-pages/SamplePage')));
+const Typography = Loadable(lazy(() => import('pages/components-overview/Typography')));
+const Color = Loadable(lazy(() => import('pages/components-overview/Color')));
+const Shadow = Loadable(lazy(() => import('pages/components-overview/Shadow')));
+const AntIcons = Loadable(lazy(() => import('pages/components-overview/AntIcons')));
+import { Routes, Route } from 'react-router-dom';
 import ThemeCustomization from 'themes';
+import MainLayout from 'layout/MainLayout/index';
 import ScrollTop from 'components/ScrollTop';
-
-// ==============================|| APP - THEME, ROUTER, LOCAL  ||============================== //
+import Loadable from 'components/Loadable';
 
 const App = () => (
   <ThemeCustomization>
     <ScrollTop>
-      <Routes />
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route element={<MainLayout />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/typography" element={<Typography />} />
+          <Route path="/color" element={<Color />} />
+          <Route path="/shadow" element={<Shadow />} />
+          <Route path="/sample-page" element={<SamplePage />} />
+          <Route path="/icons/ant" element={<AntIcons />} />
+        </Route>
+      </Routes>
     </ScrollTop>
   </ThemeCustomization>
 );
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,18 +1,30 @@
-import { useRoutes } from 'react-router-dom';
+import { useRoutes, Navigate, Route, Routes } from 'react-router-dom';
 import { lazy } from 'react';
 import Loadable from 'components/Loadable';
 import MainLayout from 'layout/MainLayout';
+import MinimalLayout from 'layout/MinimalLayout';
+
 const DashboardDefault = Loadable(lazy(() => import('pages/dashboard')));
 const SamplePage = Loadable(lazy(() => import('pages/extra-pages/SamplePage')));
 const Typography = Loadable(lazy(() => import('pages/components-overview/Typography')));
 const Color = Loadable(lazy(() => import('pages/components-overview/Color')));
 const Shadow = Loadable(lazy(() => import('pages/components-overview/Shadow')));
 const AntIcons = Loadable(lazy(() => import('pages/components-overview/AntIcons')));
-import MinimalLayout from 'layout/MinimalLayout';
-
-// render - login
 const AuthLogin = Loadable(lazy(() => import('pages/authentication/Login')));
 const AuthRegister = Loadable(lazy(() => import('pages/authentication/Register')));
+
+// Custom route guard for protected routes
+const PrivateRoute = ({ component: Component, ...rest }) => {
+  const isAuthenticated = /* Your authentication logic here */ true;
+
+  return isAuthenticated ? (
+    <Routes>
+      <Route {...rest} render={(props) => <Component {...props} />} />
+    </Routes>
+  ) : (
+    <Navigate to="/" replace />
+  );
+};
 
 export default function ThemeRoutes() {
   return useRoutes([
@@ -20,28 +32,28 @@ export default function ThemeRoutes() {
       element: <MainLayout />,
       children: [
         {
-          path: 'dashboard',
-          element: <DashboardDefault />
+          path: '/dashboard',
+          element: <PrivateRoute component={DashboardDefault} />
         },
         {
-          path: 'color',
-          element: <Color />
+          path: '/color',
+          element: <PrivateRoute component={Color} />
         },
         {
-          path: 'sample-page',
-          element: <SamplePage />
+          path: 'sample-page/*',
+          element: <PrivateRoute component={SamplePage} />
         },
         {
-          path: 'shadow',
-          element: <Shadow />
+          path: 'shadow/*',
+          element: <PrivateRoute component={Shadow} />
         },
         {
-          path: 'typography',
-          element: <Typography />
+          path: 'typography/*',
+          element: <PrivateRoute component={Typography} />
         },
         {
-          path: 'icons/ant',
-          element: <AntIcons />
+          path: 'icons/ant/*',
+          element: <PrivateRoute component={AntIcons} />
         }
       ]
     },
